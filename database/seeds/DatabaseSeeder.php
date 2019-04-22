71b4956e7f01ad866f459e4ff9d6507e24f793b8
陳膺祿 <40543236@gm.nfu.edu.tw>
2019-04-22 14:02:45 +0800

    移除不必要之migaration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
 }
